79304b86786c0ebff5f3bb206a2d6c22d328d055
Kevin <oryc9x@gmail.com>
2017-07-18 20:42:34 +0700

    fix error when loading home screen

--- src/navigation.js
+++ src/navigation.js
@@ -9,34 +9,49 @@ import Introduction from "./components/Introduction";
 import CreateAnswer from "./components/CreateAnswer";
 import Login from "./components/Login";
 
-const MainDrawerNavigator = DrawerNavigator({
-  QuestionFeed: {
-    screen: QuestionFeed,
-    navigationOptions: {
-      title: "Home"
+const MainDrawerNavigator = DrawerNavigator(
+  {
+    QuestionFeed: {
+      screen: QuestionFeed,
+      navigationOptions: {
+        title: "Feed"
+      }
+    }
+  },
+  {
+    initialRouteName: "QuestionFeed",
+    contentOptions: {
+      activeTintColor: "#e91e63"
     }
   }
-});
+);
 
 const routeConfigs = {
+  // Answer Feed Screen
   AnswerFeed: {
     screen: AnswerFeed
   },
-  QuestionFeed: {
+  // Home Screen (include: Drawer + Question Feed)
+  Home: {
     screen: MainDrawerNavigator
   },
+  // Create Question Screen
   CreateQuestion: {
     screen: CreateQuestion
   },
+  //SplashScreen
   SplashScreen: {
     screen: SplashScreen
   },
+  // Introdution screen
   Introduction: {
     screen: Introduction
   },
+  // Create Answer Screen
   CreateAnswer: {
     screen: CreateAnswer
   },
+  // Login Screen
   Login: {
     screen: Login
   }
