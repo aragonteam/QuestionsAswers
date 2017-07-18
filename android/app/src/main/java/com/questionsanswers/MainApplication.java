7d1c5c477b22f8a2b8cc92e6a118cebd9c27434b
Minh Dinh <dtminhpt@gmail.com>
2017-07-18 23:56:11 +0700

    Create a new question

--- android/app/src/main/java/com/questionsanswers/MainApplication.java
+++ android/app/src/main/java/com/questionsanswers/MainApplication.java
@@ -3,6 +3,9 @@ package com.questionsanswers;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import fr.bamlab.rnimageresizer.ImageResizerPackage;
+import com.rnfs.RNFSPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImageResizerPackage(),
+            new RNFSPackage(),
+            new ImagePickerPackage(),
             new VectorIconsPackage()
       );
     }
--- src/components/CreateQuestion.js
+++ src/components/CreateQuestion.js
@@ -1,13 +1,97 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text,StatusBar,TextInput, Image, Button } from "react-native";
+import PhotoUpload from 'react-native-photo-upload'
 
 class CreateQuestion extends Component {
   state = {};
+  static navigationOptions =  ({navigation}) => {
+        const { goToQuestionFeed } = navigation.state.params || {}
+        return {
+            title: 'Create Question',
+            headerRight: <Button title="Post" onPress={goToQuestionFeed}/>,
+        }
+    }
+ componentDidMount() {
+    let { navigation } = this.props
+    navigation.setParams({
+        goToQuestionFeed: () => this.goToQuestionFeed()
+    })
+ }
+ goToQuestionFeed(){
+        this.props.navigation.navigate('QuestionFeed')
+    }
+
+  countLetters(value) {
+      this.setState({count:value.length, text:value});
+  }
   render() {
     return (
-      <View>
-        <Text>CreateQuestion</Text>
-      </View>
+      <View style={{flex:1}}>
+            <View style={{paddingTop: 5,backgroundColor: 'white'}}>
+                <View style={{alignSelf: 'flex-end', height: 20}}>
+                    <Text
+                        style={{
+                            fontSize: 17,
+                            color: 'silver',
+                        }}>{this.state.count}
+                    </Text>
+                </View>
+                <View style={{backgroundColor:'white', width:'100%',height: 250}}>
+                  <TextInput onChangeText={(value) => this.countLetters(value) } 
+                              editable={true} multiline={true} numberOfLines={5}
+                              placeholder="Ask something"
+                              style={{margin:10, fontSize:17, 
+                                      height:'90%', paddingBottom:0, 
+                                      borderWidth: 1, borderColor: '#dcdcdc'}} />
+                </View>
+            </View>
+            <View style={{flexDirection: 'row', backgroundColor:'white'}}>
+                <View style={{backgroundColor:'white', width:'50%',height: 200}}>
+                <PhotoUpload
+                    onPhotoSelect={avatar => {
+                        if (avatar) {
+                        console.log('Image base64 string: ', avatar)
+                        }
+                    }}
+                    >
+                    <Image
+                        style={{
+                        paddingVertical: 30,
+                        width: 150,
+                        height: 150,
+                        borderRadius: 75
+                        }}
+                        resizeMode='cover'
+                        source={{
+                            uri: 'https://www.sparklabs.com/forum/styles/comboot/theme/images/default_avatar.jpg'
+                            }}
+                    />
+                </PhotoUpload> 
+            </View> 
+            <View style={{backgroundColor:'white', width:'50%',height: 200}}>
+                <PhotoUpload
+                    onPhotoSelect={avatar => {
+                        if (avatar) {
+                        console.log('Image base64 string: ', avatar)
+                        }
+                    }}
+                    >
+                    <Image
+                        style={{
+                        paddingVertical: 30,
+                        width: 150,
+                        height: 150,
+                        borderRadius: 75
+                        }}
+                        resizeMode='cover'
+                        source={{
+                            uri: 'https://www.sparklabs.com/forum/styles/comboot/theme/images/default_avatar.jpg'
+                            }}
+                    />
+                </PhotoUpload> 
+            </View> 
+        </View>   
+    </View>
     );
   }
 }
