d804bd8f78443bd501a9d2248fed0843818d6270
thaile <hoangthaitu2006@gmail.com>
2017-07-20 23:12:09 +0700

    - query question detail by question id

--- index.ios.js
+++ index.ios.js
@@ -10,6 +10,7 @@ import { AppRegistry } from "react-native";
 import App from "./src/app";
 
 export default class QuestionsAnswers extends Component {
+
   render() {
     return <App />;
   }
--- src/components/AnswerFeed.js
+++ src/components/AnswerFeed.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 
+import firebase from '../firebase/firebase';
+
 class AnswerFeed extends Component {
   state = {};
+
+  componentWillMount(){
+    var database = firebase.database();
+    firebase.database().ref('questions/1').once('value', function(snap) {
+      console.log('I fetched a question!', snap.val());
+    });
+  }
+
   render() {
     return (
       <View>
