a9d392f3fcf6deea016ad861399b2c570223873f
thaile <hoangthaitu2006@gmail.com>
2017-07-30 17:18:00 +0700

    - add blur profile UI

--- index.ios.js
+++ index.ios.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import { AppRegistry } from "react-native";
 
 import App from "./src/app";
+// import Profile from './src/components/Profile'
+import {bootstrap} from './src/util/Setup'
+// import ThemeService from './src/util/ThemeService'
+import ProfileScreenBase from './src/blur_components/ProfileScreenBase'
+
+bootstrap();
 
 export default class QuestionsAnswers extends Component {
   render() {
-    return <App />;
+    return <ProfileScreenBase />;
   }
 }
 
