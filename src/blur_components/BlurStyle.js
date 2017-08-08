df73a4ca39ddb5ceba22854cfbcee73285d402cb
thaile <hoangthaitu2006@gmail.com>
2017-08-08 23:28:38 +0700

    - update theme

--- src/blur_components/BlurStyle.js
+++ src/blur_components/BlurStyle.js
@@ -17,7 +17,8 @@ const BlurColor = {
   blurPrimary: '#00e5bf',
   lightGray: '#e0e0e0',
   blurBgWhite: 'rgba(255,255,255,0.1)',
-  blue500: '#2196f3'
+  blue500: '#2196f3',
+  blurTextStrong: 'rgba(255,255,255,0.9)'
 }
 
 export {
